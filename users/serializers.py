983a756c3062b216528f842786b3f0cbdcdc8d80
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-09 22:13:23 +0300

    serializer for user register

---
+++ users/serializers.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+
+from users.models import User
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
