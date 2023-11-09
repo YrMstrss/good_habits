910b4ec9dfe24941a425abdb0922766c39beaa9e
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-09 22:13:07 +0300

    controller for user register

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from users.serializers import UserRegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserRegisterSerializer
