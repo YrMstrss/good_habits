fe6e3c8d97e6145d916504106167f803e8d8f61a
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-10 17:55:14 +0300

    created controllers for view and edit profile of user

--- users/views.py
+++ users/views.py
@@ -1,9 +1,22 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 
-from users.serializers import UserRegisterSerializer
+from users.models import User
+from users.permissions import IsCurrentUser
+from users.serializers import UserRegisterSerializer, UserSerializer
 
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserRegisterSerializer
     permission_classes = [AllowAny]
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsCurrentUser]
