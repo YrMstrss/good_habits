bf853bb0f741dfcc0380a573d854c4542bb45c1a
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-10 17:37:23 +0300

    added permission_classes for registration

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 from users.serializers import UserRegisterSerializer
 
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserRegisterSerializer
+    permission_classes = [AllowAny]
