f30ddfc2dee9496d13324b9e1e3ee1ae696306b2
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-10 16:16:04 +0300

    added urls for getting tokens

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from users.apps import UsersConfig
 from users.views import RegisterView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
