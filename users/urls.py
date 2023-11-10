50de39ce796742ed1d843c587e41f7a5c1e72574
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-10 17:55:02 +0300

    created urls for view and edit profile of user

--- users/urls.py
+++ users/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import RegisterView
+from users.views import RegisterView, UserRetrieveAPIView, UserUpdateAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('<int:pk>', UserRetrieveAPIView.as_view(), name='view-user'),
+    path('edit/<int:pk>', UserUpdateAPIView.as_view(), name='edit-user'),
 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
