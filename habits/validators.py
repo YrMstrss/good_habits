844e6713653c580e57aed27e6adc46350c00d50f
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-11 18:28:57 +0300

    created validator to check if linked habit is nise

--- habits/validators.py
+++ habits/validators.py
@@ -9,3 +9,13 @@ class HabitTimeToCompleteValidator:
         tmp_value = dict(value).get(self.field)
         if not 0 < tmp_value <= 120:
             raise ValidationError('Время выполнения привычки должно быть больше 0 и меньше 120 секунд')
+
+
+class LinkedHabitValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        tmp_value = dict(value).get(self.field)
+        if not tmp_value.is_nice:
+            raise ValidationError('Связанная привычка должна быть приятной')
