35267a870bcfca4ee1ac69f806a86928fe151126
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-11 18:29:17 +0300

    added validator in serializer

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 
 from habits.models import Habit
-from habits.validators import HabitTimeToCompleteValidator
+from habits.validators import HabitTimeToCompleteValidator, LinkedHabitValidator
 
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = '__all__'
-        validators = [HabitTimeToCompleteValidator(field='time_to_complete')]
+        validators = [
+            HabitTimeToCompleteValidator(field='time_to_complete'),
+            LinkedHabitValidator(field='linked_habit')
+        ]
