975e14ab34d3f895347101f0836aaba9805c2692
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-13 21:51:26 +0300

    created testcase for user profile view

--- users/tests.py
+++ users/tests.py
@@ -1,9 +1,14 @@
+from datetime import timedelta
+
 from django.urls import reverse
+from django.utils.timezone import now
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
+from users.models import User
 
-class UserTestCase(APITestCase):
+
+class UserRegisterTestCase(APITestCase):
     """Тест-кейс модели пользователя """
     def setUp(self) -> None:
         self.client = APIClient()
@@ -13,23 +18,52 @@ class UserTestCase(APITestCase):
             'password': 'Ivanov123'
         }
 
-    def test_register_user(self):
 
-        """Тест регистрации пользователя"""
+class UserTestCase(APITestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
 
-        response = self.client.post(
-            reverse('users:register'),
-            data=self.data
+        self.user = User.objects.create(
+            email='ivan@ivanov.com',
+            first_name='Ivan',
+            last_name='Ivanov',
+            phone='88005553535',
+            city='Moscow'
+        )
+        self.user.set_password('Ivanov123')
+        self.user.save()
+
+        self.client.force_authenticate(user=self.user)
+
+    def test_profile_view(self):
+
+        """Тест просмотра профиля пользователя"""
+
+        response = self.client.get(
+            reverse('users:view-user', args=[self.user.id])
         )
 
         self.assertEqual(
             response.status_code,
-            status.HTTP_201_CREATED
+            status.HTTP_200_OK
         )
 
         self.assertEqual(
             response.json(),
             {
-                'email': 'ivan@gmail.com'
+                "id": 1,
+                "last_login": None,
+                "is_superuser": False,
+                "first_name": "Ivan",
+                "last_name": "Ivanov",
+                "is_staff": False,
+                "is_active": True,
+                "date_joined": (now() + timedelta(hours=3)).strftime("%d.%m.%Y %H:%M"),
+                "phone": "88005553535",
+                "city": "Moscow",
+                "avatar": None,
+                "email": "ivan@ivanov.com",
+                "groups": [],
+                "user_permissions": []
             }
         )
