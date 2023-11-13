92ecb3a5665daef4c7cfc491ff5b3ce9dec76fb2
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-13 21:37:43 +0300

    created testcase for user registration

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
 
-# Create your tests here.
+
+class UserTestCase(APITestCase):
+    """Тест-кейс модели пользователя """
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+        self.data = {
+            'email': 'ivan@gmail.com',
+            'password': 'Ivanov123'
+        }
+
+    def test_register_user(self):
+
+        """Тест регистрации пользователя"""
+
+        response = self.client.post(
+            reverse('users:register'),
+            data=self.data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+        self.assertEqual(
+            response.json(),
+            {
+                'email': 'ivan@gmail.com'
+            }
+        )
