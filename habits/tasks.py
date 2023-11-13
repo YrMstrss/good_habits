ef8256faae191438954604d34cb735d67321126a
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-14 01:55:47 +0300

    updated func 'get_chat_ids' and removed with func from tasks.py to services.py

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,17 +1,1 @@
-import requests
-import os
 
-TOKEN = os.getenv('TG_BOT_API_KEY')
-
-
-def get_chat_ids(token: str) -> list:
-    """Получает id чатов бота"""
-    get_update_url = f"https://api.telegram.org/bot{token}/getUpdates"
-    request = requests.get(get_update_url).json()
-    if request['result']:
-        chat_ids = []
-        for result in request['result']:
-            chat_ids.append(result['message']['chat']['id'])
-        return chat_ids
-
-    return []
