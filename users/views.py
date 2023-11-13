4ca291391660470d1a590901f93e50f7cf5a204e
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-14 01:56:34 +0300

    created controller getting chat_id using tg_username

--- users/views.py
+++ users/views.py
@@ -1,9 +1,15 @@
+import os
+
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views import View
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 
 from users.models import User
 from users.permissions import IsCurrentUser
 from users.serializers import UserRegisterSerializer, UserSerializer
+from users.services import get_chat_ids
 
 
 class RegisterView(generics.CreateAPIView):
@@ -20,3 +26,17 @@ class UserUpdateAPIView(generics.UpdateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = [IsCurrentUser]
+
+
+class UserGetChatIdView(View):
+    def get(self, request, pk):
+        user = User.objects.get(pk=pk)
+
+        token = os.getenv('TG_BOT_API_KEY')
+        chat_ids = get_chat_ids(token)
+        key = user.telegram_username
+        chat_id = chat_ids[key]
+        user.chat_id = chat_id
+        user.save()
+
+        return redirect(reverse_lazy('users:view-user', args=[pk]))
