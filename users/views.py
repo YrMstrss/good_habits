922a69041cefcc244aee4a545eceea390338bfb1
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-14 21:24:09 +0300

    docstrings

--- users/views.py
+++ users/views.py
@@ -13,23 +13,39 @@ from users.services import get_chat_ids
 
 
 class RegisterView(generics.CreateAPIView):
+    """
+    Контроллер для регистрации пользователя
+    """
     serializer_class = UserRegisterSerializer
     permission_classes = [AllowAny]
 
 
 class UserRetrieveAPIView(generics.RetrieveAPIView):
+    """
+    Контроллер для просмотра профиля пользователя
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
+    """
+    Контроллер для редактирования профиля пользователя
+    """
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = [IsCurrentUser]
 
 
 class UserGetChatIdView(View):
+    """
+    Контроллер для получения id чата с тг-ботом по имени пользователя телеграм
+    """
     def get(self, request, pk):
+        """
+        Метод, получающий chat_id, если такой имеется, записывающий данный id пользователю, и перенаправляющий
+        пользователя на страницу его профиля
+        """
         user = User.objects.get(pk=pk)
 
         token = os.getenv('TG_BOT_API_KEY')
