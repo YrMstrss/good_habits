5a675a195667a2658b95ce1979dbe34717898c3f
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-14 18:42:47 +0300

    created periodic task

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,1 +1,15 @@
+import os
 
+import requests
+
+from habits.models import Habit
+
+TOKEN = os.getenv('TG_BOT_API_KEY')
+
+
+def enable_notifications(obj: Habit, token=TOKEN):
+    message = f'Пора выполнить привычку: {obj}' \
+              f'Время на выполнение {obj.time_to_complete} секунд'
+
+    send_message_url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={obj.user.chat_id}&text={message}"
+    requests.get(send_message_url)
