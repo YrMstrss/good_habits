7ccdbcef0a1423d67bbbcbfe30f3bc1ae14e767a
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-14 20:25:59 +0300

    update docstring

--- habits/tasks.py
+++ habits/tasks.py
@@ -7,9 +7,12 @@ from habits.models import Habit
 TOKEN = os.getenv('TG_BOT_API_KEY')
 
 
-def enable_notifications(obj: Habit, token=TOKEN):
+def enable_notifications(obj: Habit, token=TOKEN) -> None:
     """
     Периодическая задача. Отправляет пользователю сообщение в телеграм с напоминанием о том, что пора выполнить привычку
+    :param obj: Объект типа Habit
+    :param token: API-токен чат-бота в телеграм
+    :return: None
     """
     message = f'Пора выполнить привычку: {obj}' \
               f'Время на выполнение {obj.time_to_complete} секунд'
