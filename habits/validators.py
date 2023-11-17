c876833614a9cab51b01f26172bfa39ec82bee3e
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-17 13:19:27 +0300

    changed validators

--- habits/validators.py
+++ habits/validators.py
@@ -30,29 +30,40 @@ class LinkedHabitValidator:
                 raise ValidationError('Связанная привычка должна быть приятной')
 
 
-def reward_and_linked_habit_validator(obj: Habit) -> None:
+class RewardAndLinkedHabitValidator:
 
     """
     Валидатор для проверки, что у привычки не указанны одновременно связанная привычка и вознаграждение, или не указано
     ни вознаграждения, ни связанной привычки
-    :param obj: Объект типа Habit
-    :return: None
     """
 
-    if obj.linked_habit and obj.reward:
-        raise ValidationError('У привычки не может быть одновременно и связанной привычки, и вознаграждения')
-    elif not obj.linked_habit and not obj.reward:
-        raise ValidationError('У полезной привычки должно быть или вознаграждение, или связанная привычка')
+    def __init__(self, field_1, field_2):
+        self.field_1 = field_1
+        self.field_2 = field_2
+
+    def __call__(self, value):
+        tmp_value_1 = dict(value).get(self.field_1)
+        tmp_value_2 = dict(value).get(self.field_2)
+        if tmp_value_1 and tmp_value_2:
+            raise ValidationError('У привычки не может быть одновременно и связанной привычки, и вознаграждения')
+        elif not tmp_value_1 and not tmp_value_2:
+            raise ValidationError('У полезной привычки должно быть или вознаграждение, или связанная привычка')
 
 
-def nice_habit_validator(obj: Habit):
-
+class NiceHabitValidator:
     """
     Валидатор для проверки, что у приятной привычки не указано вознаграждение или связанная привычка
-    :param obj: Объект типа Habit
-    :return: None
     """
+    def __init__(self, field_1, field_2, field_3):
+        self.field_1 = field_1
+        self.field_2 = field_2
+        self.field_3 = field_3
 
-    if obj.is_nice:
-        if obj.linked_habit or obj.reward:
-            raise ValidationError('У приятной привычки не может быть связанной привычки или вознаграждения')
+    def __call__(self, value):
+        tmp_value_1 = dict(value).get(self.field_1)
+        tmp_value_2 = dict(value).get(self.field_2)
+        tmp_value_3 = dict(value).get(self.field_3)
+
+        if tmp_value_1:
+            if tmp_value_2 or tmp_value_3:
+                raise ValidationError('У приятной привычки не может быть связанной привычки или вознаграждения')
